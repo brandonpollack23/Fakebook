7e416bc188f78b4d6849299a69c3f26d95efaa57
brandonpollack23 <brandonpollack23@gmail.com>
2015-11-18 16:35:27 -0500

    implemented put operations to add entities to the server via the listener actor

--- src/main/scala/system/F_BackBone.scala
+++ src/main/scala/system/F_BackBone.scala
@@ -2,6 +2,7 @@ package system
 
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, ActorLogging}
+import spray.http.{HttpRequest, HttpEntity}
 
 class F_BackBone extends Actor with ActorLogging {
   override def receive: Receive = ???
@@ -17,4 +18,10 @@ object F_BackBone {
   case class GetPictureInfo(id: BigInt) extends GetInfo
   case class GetAlbumInfo(id: BigInt) extends GetInfo
   case class GetImage(id: BigInt) extends GetInfo
+
+  sealed trait PutInfo
+  case class PutImage(image: HttpEntity) extends PutInfo//must send the original sender back the JSON object of the created image
+  case class CreateUser(httpRequest: HttpRequest) extends PutInfo //create user user arguments stored in httprequest and return new user JSON, they need a default profile, album, and unfilled fields for name etc
+  case class CreatePage(httpRequest: HttpRequest) extends PutInfo //create page and return JSON
+  case class CreateAlbum(httpRequest: HttpRequest) extends PutInfo //create picture and return JSON
 }
--- src/main/scala/system/F_Listener.scala
+++ src/main/scala/system/F_Listener.scala
@@ -14,53 +14,96 @@ import system.F_BackBone._
 
 import scala.util.{Failure, Success}
 
+//TODO log everything
+
 class F_Listener(backbone: ActorRef) extends Actor with ActorLogging with HttpService {
   implicit val timeout = Timeout(5 seconds)
 
   val route: Route = {
     val requester = sender
-    path("data") { id => //URIs for actual data like pictures
-      get {
+    path("data") {
+      get { id => //URIs for actual data like pictures
         genericGet(id.unmatchedPath.toString, GetImage)
-      } 
+      } ~
+      path("uploadimage") {
+        put { req =>
+          genericPut(PutImage(req.request.entity))
+        }
+      }
     } ~
-    path("user") { id =>
-      get {
+    path("user") {
+      get { id =>
         genericGet(id.unmatchedPath.toString, GetUserInfo)
+      } ~
+      path("newuser") {
+        put { req =>
+            genericPut(CreateUser(req.request))
+        }
       }
     } ~
     path("page") { id =>
       get {
         genericGet(id.unmatchedPath.toString, GetPageInfo)
+      } ~
+      path("newpage") {
+        put { req =>
+          genericPut(CreatePage(req.request))
+        }
       }
     } ~
     path("profile") { id =>
       get {
         genericGet(id.unmatchedPath.toString, GetProfileInfo)
-      }
+      } //no need for "createprofile" they are created with the user and can be accessed through the JSON returned with that creation
     } ~
     path("picture") { id =>
       get {
         genericGet(id.unmatchedPath.toString, GetPictureInfo)
-      }
+      } //same as for profile, when you upload an image the picture JSON is created
     } ~
     path("album") { id =>
       get {
         genericGet(id.unmatchedPath.toString, GetAlbumInfo)
+      } ~
+      path("createalbum") {
+        put { req =>
+          genericPut(CreateAlbum(req.request))
+        }
       }
     }
   }
 
-  def genericGet[T](id: String, messageConstructor: (BigInt) => GetInfo): Route = {
+  /**
+   * goes inside of a spray routing "get" and completes the passed message to the backbone given the id
+   * it internally converts the remaining request path to a bigint, if this fails it completes with a failure
+   * @param id the string to be converted to bigint as an ID
+   * @param messageConstructor the case class message (constructor but can be passed with sugar) to compose the bigint into
+   * @return returns a route for thed spray routing to go through and side effects the complete needed
+   */
+  def genericGet(id: String, messageConstructor: (BigInt) => GetInfo): Route = {
     try {
       val idBig = BigInt(id)
-      onComplete[T](OnCompleteFutureMagnet[T]((backbone ? messageConstructor.apply(idBig)).mapTo[T])) {
+      onComplete(OnCompleteFutureMagnet(backbone ? messageConstructor.apply(idBig))) {
         case Success(x) => complete(x)
         case Failure(ex) => complete(InternalServerError, "Request could not be completed: \n" + ex.getMessage)
       }
     } catch {
       case e: NumberFormatException =>
         complete(BadRequest, "Numbers are formatted incorrectly")
+    }
+  }
+
+  /**
+   * same as above except no need to parse a special message since the path tells all and this is for putting, so the fully constructed message gets passed here
+   * @param message constructed message to send to the backbone for handling
+   * @return
+   */
+  def genericPut(message: PutInfo) = {
+    pathEnd {
+      onComplete(OnCompleteFutureMagnet(backbone ? message)) {
+        case Success(newEntityJson) => complete(newEntityJson)
+        case Failure(ex) => complete(InternalServerError, "Error putting entity: " + ex.getMessage)
+      }
     }
   }
 
