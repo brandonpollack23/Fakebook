6cbfb20d1a2b2944f32b65d317c677d2c3e6b5fd
brandonpollack23 <brandonpollack23@gmail.com>
2015-11-22 20:34:53 -0500

    added TODO for tests to be done

---
+++ src/test/scala/FakebookTests.scala
@@ -0,0 +1,9 @@
+/* TODO
+* make tests that test the client functions ie send a command and verify the output that would be sent to the server is a correctly formatted HTTPrequest
+* Then verify a client can connect to the server and generate a listener
+* then make tests that use the client to generate one of each type of HTTP request and send those values to the server and verify the Listener forwardst the correct message to backbone
+* verify that the messages are handled correctly by each handler
+ */
+class FakebookTests {
+
+}
