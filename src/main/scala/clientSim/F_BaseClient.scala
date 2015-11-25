b9beeba0cb02b0c28053a47f3528334698203aa0
Mayank Dadheech <mayank3d@gmail.com>
2015-11-25 00:04:43 -0500

    updated baseClient, need some signature confirmation from graphnode classes

--- src/main/scala/clientSim/F_BaseClient.scala
+++ src/main/scala/clientSim/F_BaseClient.scala
@@ -47,91 +47,91 @@ def receive = {
 
   //Create operations
   case createUser =>
-    val uri = Uri("https://www.fakebook.com/newuser/") withQuery(F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
+    val uri = Uri("https://www.fakebook.com/newuser?") withQuery(F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
     sender !  (IO(Http) ? HttpRequest(PUT, uri)).mapTo[HttpResponse]
 
   case createPost(id) =>
-    val uri = Uri("https://www.fakebook.com/newpost/") withQuery(F_Post.creator -> id.toString(16),F_Post.contents -> "", F_Post.locationType -> "", F_Post.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/newpost?") withQuery(F_Post.creator -> id.toString(16),F_Post.contents -> "", F_Post.locationType -> "", F_Post.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(PUT, uri)).mapTo[HttpResponse]
 
   case createPage(id) =>
-    val uri = Uri("https://www.fakebook.com/newpage/") withQuery( F_Page.ownerID -> id.toString(16), F_Page.name -> "", F_Page.decsription -> "", F_Page.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/newpage?") withQuery( F_Page.ownerID -> id.toString(16), F_Page.name -> "", F_Page.decsription -> "", F_Page.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(PUT, uri)).mapTo[HttpResponse])
 
   case createAlbum(id) =>
-    val uri = Uri("https://www.fakebook.com/createalbum/") withQuery(F_Album.ownerString -> id.toString(16), F_Album.nameString -> "", F_Album.descriptionString -> "")
+    val uri = Uri("https://www.fakebook.com/createalbum?") withQuery(F_Album.ownerString -> id.toString(16), F_Album.nameString -> "", F_Album.descriptionString -> "")
     sender !  (IO(Http) ? HttpRequest(PUT, uri)).mapTo[HttpResponse])
 
   case uploadPicture(id) =>
-    val uri = Uri("https://www.fakebook.com/data/uploadimage/") withQuery(F_Picture.ownerString -> id.toString(16), F_Picture.nameString -> "", F_Picture.descriptionString -> "")
+    val uri = Uri("https://www.fakebook.com/data/uploadimage?") withQuery(F_Picture.ownerString -> id.toString(16), F_Picture.nameString -> "", F_Picture.descriptionString -> "")
     sender !  (IO(Http) ? HttpRequest(PUT, uri)).mapTo[HttpResponse])
 
   //Update operations
   case updateUserData(id) =>
-    val uri = Uri("https://www.fakebook.com/user/request/") withQuery(F_User.ID-> id.toString(16) ,F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
+    val uri = Uri("https://www.fakebook.com/user/request?") withQuery(F_User.ID-> id.toString(16) ,F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
     sender !  (IO(Http) ? HttpRequest(POST, uri)).mapTo[HttpResponse])
 
   case updatePictureData(id) =>
-    val uri = Uri("https://www.fakebook.com/picture/") withQuery(F_Picture.ownerString -> id.toString(16), F_Picture.nameString -> "", F_Picture.descriptionString -> "")
+    val uri = Uri("https://www.fakebook.com/picture?") withQuery(F_Picture.ownerString -> id.toString(16), F_Picture.nameString -> "", F_Picture.descriptionString -> "")
     sender !  (IO(Http) ? HttpRequest(POST, uri)).mapTo[HttpResponse])
 
   case updateAlbumData(id) =>
-    val uri = Uri("https://www.fakebook.com/album/") withQuery(F_Album.ownerString -> id.toString(16), F_Album.nameString -> "", F_Album.descriptionString -> "")
+    val uri = Uri("https://www.fakebook.com/album?") withQuery(F_Album.ownerString -> id.toString(16), F_Album.nameString -> "", F_Album.descriptionString -> "")
     sender !  (IO(Http) ? HttpRequest(POST, uri)).mapTo[HttpResponse])
 
   case updatePost(id) =>
-    val uri = Uri("https://www.fakebook.com/post/") withQuery( F_Post.creator -> id.toString(16), F_Post.contents -> "", F_Post.locationType -> "", F_Post.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/post?") withQuery( F_Post.creator -> id.toString(16), F_Post.contents -> "", F_Post.locationType -> "", F_Post.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(POST, uri)).mapTo[HttpResponse])
 
   case updatePageData(id) => //TODO distinction between multiple pages of same user by page name only??
-    val uri = Uri("https://www.fakebook.com/page/") withQuery(F_Page.name -> "", F_Page.decsription -> "", F_Page.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/page?") withQuery(F_Page.name -> "", F_Page.decsription -> "", F_Page.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(POST, uri)).mapTo[HttpResponse])
 
   //get operations
   case getUserData(id) =>
-    val uri = Uri("https://www.fakebook.com/user/") withQuery(F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
+    val uri = Uri("https://www.fakebook.com/user?") withQuery(F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
     sender !  (IO(Http) ? HttpRequest(GET, uri)).mapTo[HttpResponse])
 
   case getImageData(id) =>
-    val uri = Uri("https://www.fakebook.com/picture/") withQuery( F_Picture.ownerString -> id.toString(16), F_Picture.nameString -> "", F_Picture.descriptionString -> "")
+    val uri = Uri("https://www.fakebook.com/picture?") withQuery( F_Picture.ownerString -> id.toString(16), F_Picture.nameString -> "", F_Picture.descriptionString -> "")
     sender !  (IO(Http) ? HttpRequest(GET, uri)).mapTo[HttpResponse])
 
   case getAlbumData(id) =>
-    val uri = Uri("https://www.fakebook.com/album/") withQuery(F_Album.ownerString -> id.toString(16), F_Album.nameString -> "", F_Album.descriptionString -> "")
+    val uri = Uri("https://www.fakebook.com/album?") withQuery(F_Album.ownerString -> id.toString(16), F_Album.nameString -> "", F_Album.descriptionString -> "")
     sender !  (IO(Http) ? HttpRequest(GET, uri)).mapTo[HttpResponse])
 
   case getPost(id) =>
-    val uri = Uri("https://www.fakebook.com/post/") withQuery(F_Post.creator -> id.toString(16), F_Post.contents -> "", F_Post.locationType -> "", F_Post.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/post?") withQuery(F_Post.creator -> id.toString(16), F_Post.contents -> "", F_Post.locationType -> "", F_Post.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(GET, uri)).mapTo[HttpResponse])
 
   case getPageData(id) =>
-    val uri = Uri("https://www.fakebook.com/page/") withQuery(F_Page.ownerID -> id.toString(16) ,F_Page.name -> "", F_Page.description -> "",  F_Post.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/page?") withQuery(F_Page.ownerID -> id.toString(16) ,F_Page.name -> "", F_Page.description -> "",  F_Post.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(GET, uri)).mapTo[HttpResponse])
 
   //Delete operations
   case deleteUser(id) =>
-    val uri = Uri("https://www.fakebook.com/user/") withQuery(F_User.ID -> id.toString(16), F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
+    val uri = Uri("https://www.fakebook.com/user?") withQuery(F_User.ID -> id.toString(16), F_User.lastNameString -> "",F_User.firstNameString -> "", F_User.bioString -> "", F_User.dobString -> "", F_User.ageString -> "")
     sender !  (IO(Http) ? HttpRequest(DELETE, uri)).mapTo[HttpResponse])
 
   case deleteImage(id) =>
-    val uri = Uri("https://www.fakebook.com/picture/") withQuery(F_Picture.nameString -> "", F_Picture.descriptionString -> "", F_Picture.ownerString -> "")
+    val uri = Uri("https://www.fakebook.com/picture?") withQuery(F_Picture.nameString -> "", F_Picture.descriptionString -> "", F_Picture.ownerString -> "")
     sender !  (IO(Http) ? HttpRequest(DELETE, uri)).mapTo[HttpResponse])
 
   case deleteAlbum(id) =>
-    val uri = Uri("https://www.fakebook.com/album/") withQuery(F_Album.nameString -> "", F_Album.descriptionString -> "", F_Album.ownerString -> "")
+    val uri = Uri("https://www.fakebook.com/album?") withQuery(F_Album.nameString -> "", F_Album.descriptionString -> "", F_Album.ownerString -> "")
     sender !  (IO(Http) ? HttpRequest(DELETE, uri)).mapTo[HttpResponse])
 
   case deletePost(id) =>
-    val uri = Uri("https://www.fakebook.com/post/") withQuery(F_Post.contents -> "", F_Post.creator -> id, F_Post.locationType -> "", F_Post.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/post?") withQuery(F_Post.contents -> "", F_Post.creator -> id, F_Post.locationType -> "", F_Post.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(DELETE, uri)).mapTo[HttpResponse])
 
   case deletePage(id) =>
-    val uri = Uri("https://www.fakebook.com/page/") withQuery(F_Post.contents -> "", F_Post.creator -> id, F_Post.locationType -> "", F_Post.dateOfCreation -> "")
+    val uri = Uri("https://www.fakebook.com/page?") withQuery(F_Post.contents -> "", F_Post.creator -> id, F_Post.locationType -> "", F_Post.dateOfCreation -> "")
     sender !  (IO(Http) ? HttpRequest(GET, uri)).mapTo[HttpResponse])
 
   //Friend operations
   case sendFriendReq(id1, id2) =>
-    val uri = Uri("https://www.fakebook.com/user/resuest/") withQuery(F_User.ID -> id1.toString(16))  //send user id self and requested users
+    val uri = Uri("https://www.fakebook.com/user/resuest?") withQuery(F_User.ID -> id1.toString(16))  //send user id self and requested users
     sender !  (IO(Http) ? HttpRequest(PUT, uri)).mapTo[HttpResponse])
 
   case acceptFriendReq(id1, id2) =>
