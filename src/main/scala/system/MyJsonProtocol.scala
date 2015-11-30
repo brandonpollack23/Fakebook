8d0004bf1d2303b7787f38a769454885e00b7345
brandonpollack23 <brandonpollack23@gmail.com>
2015-11-30 14:20:09 -0500

    fixed deserialization and added time of day to date JSON

--- src/main/scala/system/MyJsonProtocol.scala
+++ src/main/scala/system/MyJsonProtocol.scala
@@ -9,15 +9,18 @@ object MyJsonProtocol extends DefaultJsonProtocol {
   //we need to provide (de)serialization methods only for Date, everything else can be implicit
   implicit object DateJsonFormat extends RootJsonFormat[Date] {
     def write(d: Date) = JsObject(
-      "month" -> JsNumber(d.getMonth),
-      "day" -> JsNumber(d.getDate),
-      "year" -> JsNumber(d.getYear)
+      "month" -> JsNumber(d.getMonth + 1),
+      "date" -> JsNumber(d.getDate),
+      "year" -> JsNumber(d.getYear + 1900),
+      "hour" -> JsNumber(d.getHours),
+      "minute" -> JsNumber(d.getMinutes),
+      "second" -> JsNumber(d.getSeconds)
     )
 
     def read(d: JsValue) = {
-      d.asJsObject.getFields("month", "day,", "year") match {
-        case Seq(JsNumber(month), JsNumber(date), JsNumber(year)) =>
-          new Date(year.toInt - 1990, month.toInt, date.toInt)
+      d.asJsObject.getFields("month", "date", "year", "hour", "minute", "second") match {
+        case Seq(JsNumber(month), JsNumber(date), JsNumber(year), JsNumber(hour), JsNumber(minute), JsNumber(second)) =>
+          new Date(year.toInt - 1900, month.toInt - 1, date.toInt, hour.toInt, minute.toInt, second.toInt)
 
         case _ => throw new DeserializationException("something wrong with deserialization of Date")
       }
